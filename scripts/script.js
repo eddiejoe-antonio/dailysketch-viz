edc3e00e6276688859f5da4d980f27682e61d0a4
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2023-01-18 17:15:01 -0500

    updated html

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-console.log("test");
-
-// Create a function that adds each image dynamically within a responsive grid
-function createImageGrid(){
-    var photo = document.createElement('img');
-    photo.src= [`${imageCounter}`].img;
-}
